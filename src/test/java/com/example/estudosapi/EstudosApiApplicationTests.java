7f3719f7bc3a474d696da1db2f87e6823d1ecc66
1danielsc <danielscolao@gmail.com>
2023-07-04 18:10:52 -0300

    first commit

---
+++ src/test/java/com/example/estudosapi/EstudosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.estudosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
